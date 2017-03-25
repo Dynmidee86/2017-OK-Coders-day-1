d9920388c7b6bbc5bb1f0687b4512daea3e964f7
Donald <donald@fcps-mbp.hsc.net.ou.edu>
2017-03-25 13:00:41 -0500

    commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// this is a comment in js
+var http = require('http')
+
+const PORT=8080;
+
+function handleRequest(reg, resp) {
+     resp.end('<h1>welcome to my webpage</h1>');
+}
+
+var server = http.createServer(handleRequest);
+
+//start our server
+server.listen(PORT,function(){
+// this is a callback, get used to it now 
+console.log("server is running on ports %s", PORT);
+});
+
+
+
